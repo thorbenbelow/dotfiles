33a62c6040612a86cc8f4725f7c5be8db4b8d2bb
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2026-09-18 23:42:38 +0200

    feat(nvim): add hardtime and precognition for vim motion training

---
+++ nvim/lua/custom/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+	"m4xshen/hardtime.nvim",
+	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	opts = {
+		disable_mouse = false, -- You might still want to use your mouse sometimes
+		hint = true,
+	},
+}
