dd76b373268d84b2675b36436f17bb3b332d8032
Thorben Below <56894536+thorbenbelow@users.noreply.github.com>
2026-09-18 22:28:52 +0200

    feat(plugins): upgrade dashboard and lua lsp config
    Add snacks.nvim to replace alpha-nvim as the dashboard.
    Add lazydev.nvim to replace the manual workspace configuration for lua_ls in nvim-lspconfig.

--- nvim/lua/custom/plugins/nvim-lspconfig.lua
+++ nvim/lua/custom/plugins/nvim-lspconfig.lua
@@ -128,7 +128,15 @@ return { -- LSP Configuration & Plugins
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
 			clangd = {},
-			gopls = {},
+			gopls = {
+				settings = {
+					gopls = {
+						env = {
+							GOOS = "linux",
+						},
+					},
+				},
+			},
 			pyright = {},
 			rust_analyzer = {},
 			jsonls = {},
@@ -144,18 +152,6 @@ return { -- LSP Configuration & Plugins
 				-- capabilities = {},
 				settings = {
 					Lua = {
-						runtime = { version = "LuaJIT" },
-						workspace = {
-							checkThirdParty = false,
-							-- Tells lua_ls where to find all the Lua files that you have loaded
-							-- for your neovim configuration.
-							library = {
-								"${3rd}/luv/library",
-								unpack(vim.api.nvim_get_runtime_file("", true)),
-							},
-							-- If lua_ls is really slow on your computer, you can try this instead:
-							-- library = { vim.env.VIMRUNTIME },
-						},
 						completion = {
 							callSnippet = "Replace",
 						},
